e5fe6f6a6a7045946778a1ce1fcb7eb934738f02
black753 <mauroo_00@hotmail.com>
2019-12-06 14:54:42 -0300

    Agregamos estilo con Bootstrap

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -13,25 +13,44 @@ if (validar_integrantes(cant_familiares)==="" && Number(cant_familiares)>0){
 	function crear_inputs(numero_familiares){
 		let i=1
 		for(i;i<=Number(numero_familiares);i++) {
-			let nodo_1=document.createElement("label")
-			nodo_1.textContent=`Edad familiar ${i}:`
-			let nodo_2=document.createElement("input")
-			nodo_2.type="number"
-			nodo_2.min=0
-			nodo_2.id="input_edad"
-			let parrafo=document.createElement("p")
+			
 
-			document.querySelector("body").appendChild(nodo_1)
-			document.querySelector("body").appendChild(nodo_2)
-			document.querySelector("body").appendChild(parrafo)
+			//Creando nodo BOOTSTRAP
+
+			const div1=document.createElement("div")
+			div1.className="input-group mb-3 col-sm-3"
+
+			const div2=document.createElement("div")
+			div2.className="input-group-prepend"
+			div1.appendChild(div2)
+
+			const span=document.createElement("span")
+			span.className="input-group-text"
+			span.id="inputGroup-sizing-default"
+			span.innerText=`Edad familiar ${i}`
+			div2.appendChild(span)
+
+			const input=document.createElement("input")
+			input.id="input_edad"
+			input.type="number"
+			input.className="form-control"
+			input["arial-label"]="Sizing example input"
+			input["aria-describedby"]="inputGroup-sizing-default"
+			div1.appendChild(input)
+
+			document.querySelector("body").appendChild(div1)
+
+
 		}
 		const boton=document.createElement("button")
 		boton.textContent="Calcular"
+		boton.className="btn btn-primary"
 		document.querySelector("body").appendChild(boton)
 
 		const boton_salario=document.createElement("button")
 		boton_salario.textContent="Agregar salarios"
 		boton_salario.id="salarios"
+		boton_salario.className="btn btn-primary"
 		document.querySelector("body").appendChild(boton_salario)
 
 		let parrafo=document.createElement("p")
@@ -63,10 +82,16 @@ if (validar_integrantes(cant_familiares)==="" && Number(cant_familiares)>0){
 			}
 			promedio=promedio/$edades.length
 
+			document.querySelector("#ra-parte").className="alert alert-primary"
+			document.querySelector("#ra-parte").role="alert"
+
 			document.querySelector("#ra-parte").textContent=`La mayor edad es ${mayor_edad} y la menor es ${menor_edad}. El promedio es ${promedio}. 
 			(Los campos no ingresados no son tenidos en cuenta)`
 		}else{
-			alert("No hay ningun campo bien ingresado, recuerda que son solo numeros")
+			
+			document.querySelector("#ra-parte").className="alert alert-danger"
+			document.querySelector("#ra-parte").role="alert"
+			document.querySelector("#ra-parte").textContent="No hay ningun campo bien ingresado, recuerda que son solo numeros"
 		}
 	}
 }else{
@@ -94,29 +119,46 @@ boton_salario.onclick=function(){
 	if (validar_integrantes(trabajadores)==="" && Number(trabajadores)+salarios_ya_creados<=cant_familiares && Number(trabajadores)>0) {
 		
 		for(let i=1;i<=Number(trabajadores);i++) {
-			let nodo_1=document.createElement("label")
-			nodo_1.textContent=`Salario anual del familiar:`
-			let nodo_2=document.createElement("input")
-			nodo_2.type="number"
-			nodo_2.min=0
-			nodo_2.id="salario"
-			nodo_2.value=1
-			let parrafo=document.createElement("p")
+			
 
-			document.querySelector("body").appendChild(nodo_1)
-			document.querySelector("body").appendChild(nodo_2)
-			document.querySelector("body").appendChild(parrafo)
+			//Creando nodo BOOTSTRAP
+
+			const div1=document.createElement("div")
+			div1.className="input-group mb-3 col-sm-3"
+
+			const div2=document.createElement("div")
+			div2.className="input-group-prepend"
+			div1.appendChild(div2)
+
+			const span=document.createElement("span")
+			span.className="input-group-text"
+			span.id="inputGroup-sizing-default"
+			span.textContent=`Salario anual del familiar:`
+			div2.appendChild(span)
+
+			const input=document.createElement("input")
+			input.type="number"
+			input.className="form-control"
+			input["arial-label"]="Sizing example input"
+			input["aria-describedby"]="inputGroup-sizing-default"
+			input.id="salario"
+			div1.appendChild(input)
+
+			document.querySelector("body").appendChild(div1)
+
 		}
 		if(document.querySelector("#calcular-salarios")==null){
 			let boton_calcular_salarios=document.createElement("button")
 			boton_calcular_salarios.textContent="Calcular salarios"
 			boton_calcular_salarios.id="calcular-salarios"
+			boton_calcular_salarios.className="btn btn-primary"
 			document.querySelector("body").appendChild(boton_calcular_salarios)
 		}else{
 			document.querySelector("#calcular-salarios").remove()
 			let boton_calcular_salarios=document.createElement("button")
 			boton_calcular_salarios.textContent="Calcular salarios"
 			boton_calcular_salarios.id="calcular-salarios"
+			boton_calcular_salarios.className="btn btn-primary"
 			document.querySelector("body").appendChild(boton_calcular_salarios)
 		}
 
@@ -125,11 +167,23 @@ boton_salario.onclick=function(){
 		mostrar_calculos_salarios()
 	}else{
 		if(validar_integrantes(trabajadores)!==""){
-			alert(validar_integrantes(trabajadores))
+
+			document.querySelector("#da-parte").className="alert alert-danger"
+			document.querySelector("#da-parte").role="alert"
+			document.querySelector("#da-parte").textContent=validar_integrantes(trabajadores)
+
 		}else if(!(Number(trabajadores)+salarios_ya_creados<=cant_familiares)){
-			alert("No puede haber mas trabajadores que integrantes")
+
+			document.querySelector("#da-parte").className="alert alert-danger"
+			document.querySelector("#da-parte").role="alert"
+			document.querySelector("#da-parte").textContent="No puede haber mas trabajadores que integrantes"
+
 		}else{
-			alert("No ingresaste ningun numero valido")
+			
+			document.querySelector("#da-parte").className="alert alert-danger"
+			document.querySelector("#da-parte").role="alert"
+			document.querySelector("#da-parte").textContent="No ingresaste ningun numero valido"
+			
 		}
 	}
 
@@ -142,21 +196,25 @@ boton_salario.onclick=function(){
 
 function mostrar_calculos_salarios(){
 	const $boton_calcular_salarios=document.querySelector("#calcular-salarios")
-	let $salarios=[]
+
 
 	$boton_calcular_salarios.onclick=function(){
 
 
 		let salarios_codigo=document.querySelectorAll('#salario')
+		let $salarios=[]
+
 
 		for(i=0;i<salarios_codigo.length;i++){
-			$salarios.push(Number(salarios_codigo[i].value))
+			if(salarios_codigo[i].value!=""){
+				$salarios.push(Number(salarios_codigo[i].value))
+			}
 		}
 
-		if(validar_inputs($salarios)){
+		if(validar_inputs($salarios) && $salarios.length>0){
 
 			const mayor_salario=Math.max.apply(null,$salarios)
-			const menor_salario=Math.min.apply(null,$salarios)
+			const menor_salario=Math.min.apply(null,$salarios)			//Bugs con el salario
 			let salario_mensual_promedio
 			function F_salario_mensual_promedio(salarios_anuales){
 				let salarios_totales=0
@@ -169,11 +227,18 @@ function mostrar_calculos_salarios(){
 
 			F_salario_mensual_promedio($salarios)
 
+			document.querySelector("#da-parte").className="alert alert-primary"
+			document.querySelector("#da-parte").role="alert"
+
 			document.querySelector("#da-parte").textContent=`El mayor salario anual es de $${new Intl.NumberFormat().format(mayor_salario)},
 			 el menor es de $${new Intl.NumberFormat().format(menor_salario)}. El salario mensual promedio de todo
-			 es de $${new Intl.NumberFormat().format(salario_mensual_promedio)}`
+			 es de $${new Intl.NumberFormat().format(salario_mensual_promedio)} (Los campos no ingresados no son tenidos en cuenta)`
 		}else{
-			alert("Los salarios deben ser mayores a $0")
+			//alert("Los salarios deben ser mayores a $0")
+
+			document.querySelector("#da-parte").className="alert alert-danger"
+			document.querySelector("#da-parte").role="alert"
+			document.querySelector("#da-parte").textContent="Debes completar los campos con numeros mayores a 0"
 		}
 	}	
 }
@@ -184,7 +249,7 @@ function validar_integrantes(integrantes){
 	}if (/[a-z]/i.test(integrantes)){
 		return "Solo se puede ingresar numeros"
 	}if(integrantes>25){
-		return "Demasiados familiares"
+		return "No se admiten numero demasiados grandes"
 	}if(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(integrantes)){
 		return "No se admiten signos"
 	}
@@ -196,8 +261,10 @@ function validar_inputs(array_numero){
 	let inputs_correctos=true
 
 	array_numero.forEach(function(e){
-		if(e==="" || e<=0){
+		if(e<1){
 			inputs_correctos=false
+		}else{
+			inputs_correctos=true
 		}
 
 	})
